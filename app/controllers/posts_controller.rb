4291ffcbed82fd9fc14aa4106ffb254332630c21
umepab <umepab@yandex.ru>
2017-05-25 12:04:43 +0200

    без регистрациии еще

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+
 
   end
 
@@ -23,21 +26,28 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def update
-    @post = Post.find(params[:id])
-      if @post.update_attributes(post_params)
+  if @post.update_attributes(post_params)
       redirect_to @post
       else
         render :edit
-
-
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :summary, :body)
   end
